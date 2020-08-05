30e864c8c7794f4aee9ad650080c7df497a28e1c
IndyLogix Solutions <info@indylogix.com>
2020-08-05 15:28:34 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Lottie Demo"),
         ),
+
         //Animation is
         body: Center(
           child: Padding(
